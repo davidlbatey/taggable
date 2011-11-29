3169f4e36c8871be92f26176a7737c30be9eb024
David Batey <davidlbatey@gmail.com>
2011-11-29 22:40:33 +0000

    Allow the same tag name on different models

--- lib/taggable.rb
+++ lib/taggable.rb
@@ -3,19 +3,19 @@ module Taggable
     def tag_list
       tags.collect(&:name).join(",")
     end
- 
+
     def tag_list=(tag_str)
       self.tags = []
-      split_tags(tag_str).collect {|n| self.tags << Tag.find_or_create_by_name(n)}
+      split_tags(tag_str).collect {|n| self.tags << Tag.find_or_create_by_taggable_id_and_name(self.id, n)}
     end
   end
-  
-  
+
+
   def split_tags tag_str
     return [] if tag_str.empty?
     tag_str.split(",").collect(&:strip)
   end
-  
+
   def self.included klass
     klass.class_eval do
       many :tags, :as => :taggable
